216c7dc459715e6f04bd4bf59b5b3f491648a7bf
Russ Brown <russbrown@protonmail.com>
2018-03-02 00:12:00 -0700

    Set ALLOWED_HOSTS and disabled debugging

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".memit.ca", ".linux-test.duckdns.org"]
 
 
 # Application definition
