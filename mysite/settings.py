8736db0c6b1a12b80426bd37c33e439f65385620
Russ Brown <russbrown@protonmail.com>
2018-03-01 23:36:02 -0700

    Switched from Sqlite3 to Postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'memit',
+        'USER': 'russ',
+        'PASSWORD': POSTGRESQL_DB_PWD,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
